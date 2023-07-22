19371013f8d779fafa8bee70deedda33b2f2b754
Anigbogu Onyinyechukwu Lilian <50427805+ZennyAOL@users.noreply.github.com>
2023-07-22 21:46:38 +0100

    task 1 correction

--- python-hello_world/1-edges.py
+++ python-hello_world/1-edges.py
@@ -4,5 +4,5 @@ word_first_3 = word [:3]
 print("First 3 letters: {}".format(word_first_3))
 word_last_2 = word [-2:]
 print("Last 2 letters: {}".format(word_last_2))
-middle_word = word [-8:-1]
+middle_word = word [1:-1]
 print("Middle word: {}".format(middle_word))
