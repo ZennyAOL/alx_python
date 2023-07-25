1d5c08648cc7285ef7c56e904bd55b93da33ed17
Anigbogu Onyinyechukwu Lilian <50427805+ZennyAOL@users.noreply.github.com>
2023-07-25 11:13:11 +0100

    Task 6 modified 2

--- python-hello_world/6-print_comb3.py
+++ python-hello_world/6-print_comb3.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+combinations = []
 for tens_digit in range(10):
     for ones_digit in range(tens_digit + 1, 10):
-        print(", ".join("{:02d}".format(tens_digit * 10 + ones_digit) for tens_digit in range(10) for ones_digit in range(tens_digit + 1, 10)))
+        combinations.append("{:02d}".format(tens_digit * 10 + ones_digit))
+
+result = ", ".join(combinations)
+print(result)
 
