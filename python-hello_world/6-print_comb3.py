875235d8531987a3eaeb9410c549071f78286444
Anigbogu Onyinyechukwu Lilian <50427805+ZennyAOL@users.noreply.github.com>
2023-07-25 11:04:18 +0100

    Task 6 modified

--- python-hello_world/6-print_comb3.py
+++ python-hello_world/6-print_comb3.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 for tens_digit in range(10):
     for ones_digit in range(tens_digit + 1, 10):
-        print("{:02d}".format(tens_digit * 10 + ones_digit), end=", " if ones_digit < 9 else "\n")
+        print(", ".join("{:02d}".format(tens_digit * 10 + ones_digit) for tens_digit in range(10) for ones_digit in range(tens_digit + 1, 10)))
+
